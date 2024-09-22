f5cadb9060213b7b72db14c33b0ce3fba1a7ee7c
ihourglass <inversionhourglass@outlook.com>
2024-09-22 16:57:14 +0800

    Add a new type matcher TypeReferenceMatcher.

---
+++ Matchers/TypeReferenceMatcher.cs
@@ -0,0 +1,35 @@
+﻿using Mono.Cecil;
+
+namespace Cecil.AspectN.Matchers
+{
+    public class TypeReferenceMatcher : ITypeMatcher
+    {
+        private readonly TypeReference _typeRef;
+        private readonly TypeDefinition? _typeDef;
+
+        public TypeReferenceMatcher(TypeReference typeRef)
+        {
+            _typeRef = typeRef;
+            if (typeRef is TypeDefinition typeDef)
+            {
+                _typeDef = typeDef;
+            }
+        }
+
+        public bool IsMatch(TypeSignature signature)
+        {
+            var typeRef = signature.Reference;
+            if (_typeDef != null)
+            {
+                if (typeRef is not TypeDefinition typeDef)
+                {
+                    typeDef = typeRef.Resolve();
+                }
+
+                return _typeDef == typeDef;
+            }
+
+            return _typeRef.Module == typeRef.Module && _typeRef.FullName == typeRef.FullName;
+        }
+    }
+}
