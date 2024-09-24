32f7e44299bb5d7715fb214cc908e6cf0fa36c9e
ihourglass <inversionhourglass@outlook.com>
2024-09-24 23:18:47 +0800

    Add a cached IMatcher type CachedMatcher and a cached ITypeMatcher type CachedTypeMatcher.

---
+++ Matchers/CachedMatcher.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace Cecil.AspectN.Matchers
+{
+    public class CachedMatcher(IMatcher matcher) : IMatcher
+    {
+        private static readonly Dictionary<Pair, bool> _Cache = [];
+
+        public ITypeMatcher DeclaringTypeMatcher { get; } = matcher.DeclaringTypeMatcher is CachedTypeMatcher cachedTypeMatcher ? cachedTypeMatcher : new CachedTypeMatcher(matcher.DeclaringTypeMatcher);
+
+        public bool IsMatch(MethodSignature signature)
+        {
+            var pair = new Pair(matcher, signature);
+            if (!_Cache.TryGetValue(pair, out var result))
+            {
+                result = matcher.IsMatch(signature);
+                _Cache[pair] = result;
+            }
+
+            return result;
+        }
+
+        readonly struct Pair(IMatcher matcher, MethodSignature signature)
+        {
+            public readonly IMatcher Matcher = matcher;
+
+            public readonly MethodSignature Signature = signature;
+        }
+    }
+
+    public static class CachedMatcherExtensions
+    {
+        public static IMatcher Cached(this IMatcher matcher)
+        {
+            return new CachedMatcher(matcher);
+        }
+    }
+}
---
+++ Matchers/CachedTypeMatcher.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Cecil.AspectN.Matchers
+{
+    public class CachedTypeMatcher(ITypeMatcher typeMatcher) : ITypeMatcher
+    {
+        private static readonly Dictionary<Pair, bool> _Cache = [];
+
+        public bool IsMatch(TypeSignature signature)
+        {
+            var pair = new Pair(typeMatcher, signature);
+            if (!_Cache.TryGetValue(pair, out var result))
+            {
+                result = typeMatcher.IsMatch(signature);
+                _Cache[pair] = result;
+            }
+
+            return result;
+        }
+
+        readonly struct Pair(ITypeMatcher matcher, TypeSignature signature)
+        {
+            public readonly ITypeMatcher Matcher = matcher;
+
+            public readonly TypeSignature Signature = signature;
+        }
+    }
+
+    public static class CachedTypeMatcherExtensions
+    {
+        public static ITypeMatcher Cached(this ITypeMatcher typeMatcher)
+        {
+            return new CachedTypeMatcher(typeMatcher);
+        }
+    }
+}
